339cd0bfa6ac4d0aa04b0d93f7429fa0dbec2f81
aalku <aalku7@gmail.com>
2023-07-20 12:27:21 +0000

    Temperature (custom) monitorization
    Telegraf update and configuration, with python3 support.

---
+++ query_temp.py
@@ -0,0 +1,39 @@
+# sudo apt install python3-full
+
+import json
+import datetime
+import sys
+
+try:
+
+  hours = 72
+
+  f = open("/var/log/fan-controller.log", "r")
+  try:
+      lines = f.readlines()
+  finally:
+      f.close()
+
+  data = []
+
+  start = datetime.datetime.now() - datetime.timedelta(hours=hours)
+
+  def json_serialize(obj):
+    if isinstance(obj, datetime.datetime):
+        return obj.isoformat()
+    raise TypeError ("Type %s not serializable" % type(obj))
+
+  for l in lines:
+    d = json.loads(l)
+    try:
+        dt = datetime.datetime.fromisoformat(d["updated-ts"])
+        if dt >= start:
+            temp = d["temp-c"]
+            fan = int(100 if bool(d["state"]) else 0)
+            data.append({"ts":dt, "fan":fan, "temp":temp})
+    except:
+        None
+
+  print (json.dumps(data, default=json_serialize))
+except Exception as e:
+  print(e, file=sys.stderr)
--- run.sh
+++ run.sh
@@ -8,8 +8,10 @@ INFLUXDB_DATAPATH="/var/lib/influxdb2"
 
 INFLUX_URL="http://host.containers.internal:8086"
 
-TELEGRAF_IMAGE="docker.io/telegraf:1.9"
+# TELEGRAF_IMAGE="docker.io/telegraf:1.27.2"
+TELEGRAF_IMAGE="telegraf-custom"
 TELEGRAF_CONFIG="$PWD/telegraf.conf"
+TELEGRAF_CONFIG_TEMP_PYTHON_SCRIPT="$PWD/query_temp.py"
 
 # Load secrets
 . secret.conf
@@ -19,6 +21,10 @@ podman stop grafana > /dev/null 2>&1
 podman stop influxdb > /dev/null 2>&1
 podman stop telegraf > /dev/null 2>&1
 
+# build
+cd telegrafImage
+podman build -t telegraf-custom .
+cd -
 
 # run influxdb
 #  -e "INFLUXDB_HTTP_ENABLED=true" \
@@ -31,11 +37,21 @@ podman run -p 8086:8086 --name=influxdb -d \
   "$INFLUXDB_IMAGE" \
   --reporting-disabled
 
-# run telegraf
-#  -p 8125:8125/udp -p 8092:8092/udp -p 8094:8094
-podman run --name=telegraf -d \
+# run grafana
+#  -e "GF_PATHS_CONFIG=/var/lib/grafana/grafana.ini" \
+mkdir "$GRAFANA_DATAPATH" > /dev/null 2>&1
+podman run -p 3000:3000 --name=grafana -d \
   --rm \
   --user "$(id -u)" \
+  --volume "$GRAFANA_DATAPATH:/var/lib/grafana" \
+  "$GRAFANA_IMAGE"
+
+# run telegraf
+#  -p 8125:8125/udp -p 8092:8092/udp -p 8094:8094
+podman run --name=telegraf \
+  --rm -d \
+  --user "$(id -u)" \
+  --cap-add cap_net_raw \
   -v "$TELEGRAF_CONFIG":/etc/telegraf/telegraf.conf:ro \
   -v /:/hostfs:ro \
   -e HOST_ETC=/hostfs/etc \
@@ -47,15 +63,8 @@ podman run --name=telegraf -d \
   -e INFLUX_URL="$INFLUX_URL" \
   -e INFLUX_TOKEN="$INFLUX_TOKEN" \
   -e HOSTNAME="$HOSTNAME" \
+  -v "$TELEGRAF_CONFIG_TEMP_PYTHON_SCRIPT:/var/query_temp.py" \
+  -e TELEGRAF_CONFIG_TEMP_PYTHON_SCRIPT="python3 /var/query_temp.py" \
+  -v /var/log/fan-controller.log:/var/log/fan-controller.log:ro \
   -v /var/run/podman/podman.sock:/var/run/podman/podman.sock \
   "$TELEGRAF_IMAGE"
-
-# run grafana
-#  -e "GF_PATHS_CONFIG=/var/lib/grafana/grafana.ini" \
-mkdir "$GRAFANA_DATAPATH" > /dev/null 2>&1
-podman run -p 3000:3000 --name=grafana -d \
-  --rm \
-  --user "$(id -u)" \
-  --volume "$GRAFANA_DATAPATH:/var/lib/grafana" \
-  "$GRAFANA_IMAGE"
-
